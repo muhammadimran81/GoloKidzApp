b7e4d7014d4d233dbc6a1b51721f2b3d37263d86
muhammadimrankhalid <muhammadimrankhalid@hotmail.com>
2025-09-10 22:34:48 +0300

    added caliduary

--- KidzForGolo/settings.py
+++ KidzForGolo/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = "KidzForGolo.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.postgresql",
-#        "NAME": os.getenv("DB_NAME"),
-#        "USER": os.getenv("DB_USER"),
-#        "PASSWORD": os.getenv("DB_PASSWORD"),
-#        "HOST": os.getenv("DB_HOST", "localhost"),
-#        "PORT": os.getenv("DB_PORT", "5432"),
-        
-#    }
-#}
-
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+        
+    }
 }
+
+#DATABASES = {
+#    "default": dj_database_url.config(
+#        default=os.environ.get("DATABASE_URL")
+#    )
+#}
 
 
 # Password validation
